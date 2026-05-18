03981baec7b7b858b4c5d4dbc6223083d1c4f99e
FilipeBento21 <filipebentoheliodoro@gmail.com>
2026-05-17 23:32:30 -0300

    feat: iniciando projeto e ajustando arquivos para início do código

---
+++ src/test/java/com/filipe_bento/agendamento_barbearia/AgendamentoBarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe_bento.agendamento_barbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoBarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
